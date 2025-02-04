05ba938a9f73a13098e978ec605f1faacd965329
AryanVerma2409 <343vermaaryan@gmail.com>
2025-02-04 21:06:55 +0530

    SB

--- Frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ Frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/Searchbar";
 
 
 
 const Navbar = () => {
+    const[searchQuery,setSearchQuery]=useState("");
 
     const navigate = useNavigate;
     const onLogout= ()=> {
         navigate("/login")
     }
+
+    const handelSearch=()=>{};
+
+    // const onClearSearch=()=>{
+    //     setSearchQuery("");
+    // };
      return(
         <div className="bg-white flex items-center justify-between px-6 py-4 drop-shadow">
             <h2 classname="text-xl font-medium text-black py-2 center">Notes</h2>
-
+            <SearchBar   value={searchQuery}
+                onChange={({ target })=>{
+                    setSearchQuery(target.value);
+                }}
+                handelSearch={handelSearch}
+                //onClearSearch={onClearSearch}
+            />
             <ProfileInfo onLogout={onLogout} />
+            
         </div>
         
      )
--- Frontend/notes-app/src/components/SearchBar/Searchbar.jsx
+++ Frontend/notes-app/src/components/SearchBar/Searchbar.jsx
@@ -1,1 +1,18 @@
 import React from "react";
+import{FaMagnifyingGlass} from "react-icons/fa6";
+                                                    // comment this onclearSearch    coz it is not in use   (search bar par cross icon)
+const SearchBar=({ value ,onChange ,handelSearch, onClearSearch})=>{
+    return(
+        <div className="w-80 flex items-center px-4 bg-slate-100 rounded-md">
+            <input
+            type="text"
+            placeholder="Search Notes"
+            className="w-full text-xs bg-transparent py-[11px] outline-none"
+            value={value}
+            onChange={onChange}
+            />
+            <FaMagnifyingGlass className="text-slate-400 cursor-pointer hover:text-black"/>
+        </div>
+    )
+}
+export default SearchBar;
